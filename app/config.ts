cd28362eaf0f381b47750819a6e7dd3f80ee3cdb
EveryJE <kgyan19lf@outlook.com>
2025-08-14 05:22:11 -1200

    feat:guest land

--- app/config.ts
+++ app/config.ts
@@ -4,15 +4,16 @@ const config = {
   geoCodingApi: "https://nominatim.openstreetmap.org",
   routingApi: "https://router.project-osrm.org/route/v1",
   mapConfig: {
-    center: [-2.0019, 5.2974],
-    zoom: 16,
+    center: [-2.0004313035171037,
+      5.298313460071185],
+    zoom: 18,
     bearing: -15,
     pitch: 45,
-    maxBounds :[[-2.00454, 5.29415], [-1.98929, 5.30060] 
+    maxBounds: [[-2.00454, 5.29415], [-1.98929, 5.30060]
     ]
   } as maplibregl.MapOptions,
   mapStyles: {
-  light: "https://tiles.openfreemap.org/styles/bright",
+    light: "https://tiles.openfreemap.org/styles/bright",
     dark: "/styles/dark/style.json",
   },
 };
